544302b62ee27bb7f1765c9e0aba6593709cc526
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-01-31 13:33:49 +0100

    nicer styling of the color palett

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const colorPickerForm = document.getElementById("color-picker-form")
 const colorInput = document.getElementById("color-input")
 const schemeOptions = document.getElementById("scheme-options")
+const allColorsBox = document.getElementById("all-colors-box")
 
 colorPickerForm.addEventListener("submit", (e) => {
     e.preventDefault(); //ensures the color does not disappear straight a way
@@ -11,5 +12,17 @@ colorPickerForm.addEventListener("submit", (e) => {
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&format=json&mode=${chosenScheme}&count=6`)
     .then(res => res.json())
-    .then(data => {console.log(data)})
+    .then(data => {
+        console.log(data)
+        let html = ""
+        for(let oneColor of data.colors){
+            html += `
+            <div class="one-color-components">
+                <div class="one-color-box" style="background-color:${oneColor.hex.value}"></div>
+                <p>${oneColor.hex.value}</p>
+            </div>
+            `
+        }
+        allColorsBox.innerHTML = html
+    })
 })
